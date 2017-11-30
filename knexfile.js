a3142734f0f8a5b07edcd8fc9f01d8ba8836a05b
Lola Brenner <amandadbrenner@gmail.com>
2017-11-30 14:37:07 -0700

    adds seed for testing

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds/test'
     },
     useNullAsDefault: true
   }
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -2,3 +2,14 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
+
+chai.use(chaiHttp);
+
+
+describe('Client Routes', () => {
+
+});
+
+describe('API Routes', () => {
+
+});
