4435bb29c628d67fff34c6264a7387eb6e9e9c12
Lola Brenner <amandadbrenner@gmail.com>
2017-12-04 09:29:14 -0700

    ci setup

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/palettes_test',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/palettes_test',
     migrations: {
       directory: './db/migrations'
     },
