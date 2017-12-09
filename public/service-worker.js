0641588ec6c9d320c77d23423e0d31a47806028c
Lola Brenner <amandadbrenner@gmail.com>
2017-12-09 10:46:04 -0700

    adds service worker

---
+++ public/service-worker.js
@@ -0,0 +1,76 @@
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('assets-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/scripts/scripts.js',
+        '/css/styles.css',
+        '/images/garbage.png',
+        '/images/art-supplies.jpg',
+        '/images/lock-open.svg',
+        '/images/lock.svg',
+        '/images/palette.png'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+this.addEventListener('activate', (event) => {
+  let cacheWhitelist = ['assets-v1'];
+
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+      .then(() => clients.claim())
+  );
+});
+//
+// let pendingMarkdowns = [];
+//
+// this.addEventListener('message', (event) => {
+//   if (event.data.type === 'add-markdown') {
+//     pendingMarkdowns.push(event.data.markdown);
+//     self.registration.sync.register('addMarkdown')
+//   }
+// });
+//
+// this.addEventListener('sync', (event) => {
+//   if (event.tag === 'addMarkdown') {
+//     event.waitUntil(postPendingMarkdowns()
+//       .then(responses => {
+//         self.clients.matchAll().then(clients => {
+//           clients[0].postMessage({ type: 'markdowns-synced' });
+//         });
+//         self.registration.showNotification("New markdowns synced!");
+//       })
+//       .catch(error => console.error(error))
+//     );
+//   }
+// });
+//
+// const postPendingMarkdowns = () => {
+//   let markdownPromises = pendingMarkdowns.map((markdown) => {
+//     return fetch('/api/v1/markdowns', {
+//       method: 'POST',
+//       body: JSON.stringify(markdown),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     })
+//   })
+//
+//   return Promise.all(markdownPromises);
+// };
